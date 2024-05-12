b00c67d95060d5ed3f699290e6e50c30d5c054e6
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-05-12 12:56:39 -0500

    finished array updating

--- Static pages in React/update ARRAYS in state/src/MyComponent.jsx
+++ Static pages in React/update ARRAYS in state/src/MyComponent.jsx
@@ -3,7 +3,43 @@ import React, {useState} from "react"
 
 function MyComponent(){
 
-    return(<></>)
+    const [foods , setFood ] = useState(["Apple", "orange", "banana"])
+
+    function handleAddFood()
+    {
+        const newfood = document.getElementById("foodInput").value
+        document.getElementById("foodInput").value = ""
+        setFood(f => [...f , newfood])
+    }
+
+
+    
+    function handleRemoveFood(index)
+    {
+        // _ means ignore that parameter 
+        setFood(foods.filter((_, i ) => i != index))
+        
+    }
+
+
+
+    return(
+            <div>
+                <h2>List of food</h2>
+
+                <ul>
+                    {/* array.mapMethod((parameters) => functionality) */}
+                    {foods.map((food, index) =>
+                         <li key={index}  onClick={() => handleRemoveFood(index)}>
+                            {food}
+                         </li>)}
+                </ul>
+
+                <input type="text" id="foodInput" placeholder="Enter food name" />
+                <button onClick={handleAddFood}>Add Food </button>
+
+            </div>
+          )
 
 }
 
