f3b9db905c8403dfe9a63772d3a8c970251e1de1
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-05-13 15:15:20 -0500

    finished making the app

--- Static pages in React/digital clock app/src/DigitalClock.jsx
+++ Static pages in React/digital clock app/src/DigitalClock.jsx
@@ -17,16 +17,20 @@ function DigitalClock(){
 
     function formatTime()
     {
-        const hours = time.getHours()
+        let hours = time.getHours()
         const minutes = time.getMinutes()
         const seconds = time.getSeconds()
         const meridiem = hours >=12 ? "PM" : "AM"
 
         hours = hours % 12 || 12 
 
-        return(`${hours}:${minutes}:${seconds} ${meridiem}`)
+        return(`${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`)
     }
 
+    function padZero(number)
+    {
+        return (number < 10 ? "0" : "") + number
+    }
 
 
     return(<div className="clock-container">
